668ca00206facf8e71215f64a2ec16dcf8acf451
clintonhpat <118099686+clintonhpat@users.noreply.github.com>
2024-06-12 21:54:44 -0500

    Update PortfolioPage.js

--- src/components/PortfolioPage.js
+++ src/components/PortfolioPage.js
@@ -13,7 +13,9 @@ export default function PortfolioPage() {
         link1: 'https://github.com/clintonhpat/react-to-do-list',
         link2: 'https://reacttodolist.onrender.com/',
         image: todoListPic,
-        cardNum: '1'
+        cardNum: '1',
+        hasQA: false,
+        qaLink: 'https://your-test-cases-link1.com'
     }
     const project2 = {
         header: 'Netflix Clone',
@@ -21,34 +23,40 @@ export default function PortfolioPage() {
         link1: 'https://github.com/clintonhpat/netflix-clone-react',
         link2: 'https://netflixclonereact.onrender.com/',
         image: notflix,
-        cardNum: '2'
+        cardNum: '2',
+        hasQA: true,
+        qaLink: 'https://docs.google.com/document/d/1Ipf4XDVp13ce9CMBpkj25CwcnZtcLqAL/edit?usp=drive_link&ouid=117839534229967755259&rtpof=true&sd=true'
     }
     const project3 = {
-      header: 'CRUD Zoology',
-      content: 'A Zoology Crud database of Animals and their respective classes.',
-      link1: 'https://github.com/clintonhpat/crud-zoology',
-      link2: 'https://crudzoologyapp.onrender.com',
-      image: crudZoologyPic2,
-        cardNum: '3'
+        header: 'CRUD Zoology',
+        content: 'A Zoology Crud database of Animals and their respective classes.',
+        link1: 'https://github.com/clintonhpat/crud-zoology-api/tree/main/ZoologyCRUDApi',
+        link2: 'https://crudzoologyapp.onrender.com',
+        image: crudZoologyPic2,
+        cardNum: '3',
+        hasQA: true,
+        qaLink: 'https://docs.google.com/document/d/1zheDnZpzSHG9w0Qe9rvMLj5lDsWd2Zmw/edit?usp=sharing&ouid=117839534229967755259&rtpof=true&sd=true'
     }
     const project4 = {        
         header: 'Ticket Sys',
         content: 'An app to assist companies in keeping track of tasks/employee activity',
-        link1: 'https://github.com/clintonhpat/BillieJeansAuto',
+        link1: 'https://github.com/clintonhpat/BillieJeansAuto-Api',
         link2: 'https://billiejeansauto.onrender.com',
         image: billieJeansAutoPic,
-        cardNum: '4'
+        cardNum: '4',
+        hasQA: true,
+        qaLink: 'https://docs.google.com/document/d/1N7AIbKs5oV00kn4DtfWmITtdef5rM482/edit?usp=drive_link&ouid=117839534229967755259&rtpof=true&sd=true'
     }
     const projects = useRef([project1, project2, project3, project4]);
 
-  return (
-    <>
-    <section className="section-transform hide-section portfolio active" id="portfolio">
-        <div className="main-title">
-          <h2>Proj<span>ects</span><div className="bg-text">projects</div></h2>
-        </div>
-        <div className="projects-section"><CardList projects={projects.current} /></div>
-    </section>
-    </>
-  )
+    return (
+        <>
+            <section className="section-transform hide-section portfolio active" id="portfolio">
+                <div className="main-title">
+                    <h2>Proj<span>ects</span><div className="bg-text">projects</div></h2>
+                </div>
+                <div className="projects-section"><CardList projects={projects.current} /></div>
+            </section>
+        </>
+    )
 }
